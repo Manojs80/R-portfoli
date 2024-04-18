a6e4a5e26b9badac9322574966363c97b0752033
MANOJ S <08041996kallada@gmail.com>
2024-04-18 21:40:03 +0530

    initial

---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Experience = () => {
+  return (  
+      <div className='bg-warning container-fluid'>
+       <div >
+        <img className='w-100' src="https://www.slogansbuddy.com/wp-content/uploads/2022/03/Freshman-1-.jpg" alt="loading" ></img>
+       </div>
+      </div>
+  )
+}
+
+export default Experience
